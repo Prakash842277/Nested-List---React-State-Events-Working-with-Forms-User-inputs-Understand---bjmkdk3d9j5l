6f18ea177a9fde6ca543ff443661b2ac1e465ec8
Prakash842277 <121437397+Prakash842277@users.noreply.github.com>
2023-03-07 15:15:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component, useState } from "react";
 import "./../styles/App.css";
+import Province from './Province'
+
 
 // Do not alter the states const and values inside it.
 const states = [
@@ -155,7 +157,183 @@ const states = [
 ];
 
 function App() {
-  return <div id="main"></div>;
+  const [state, setState] = useState(states)
+
+  return (
+  <div id="main">
+  <ul>
+    {
+      state.map(item=>(
+        <Province
+        index={state.indexOf(item)} 
+        name={item.name}
+        cities={item.cities}  />
+      ))
+    }
+    </ul>
+  </div>
+  );
 }
 
 export default App;
+// import React, { Component, useState } from "react";
+// import "./../styles/App.css";
+
+// // Do not alter the states const and values inside it.
+// const states = [
+//   {
+//     name: "Madhya Pradesh",
+//     cities: [
+//       {
+//         name: "Indore",
+//         towns: [
+//           {
+//             name: "Mhow",
+//           },
+//           {
+//             name: "Dewas",
+//           },
+//         ],
+//       },
+//       {
+//         name: "Bhopal",
+//         towns: [
+//           {
+//             name: "Manit",
+//           },
+//           {
+//             name: "Berasia",
+//           },
+//         ],
+//       },
+//       {
+//         name: "Gwalior",
+//         towns: [
+//           {
+//             name: "Ajaypur",
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     name: "Jharkhand",
+//     cities: [
+//       {
+//         name: "Dhanbad",
+//         towns: [
+//           {
+//             name: "IIT(ISM) Dhanbad",
+//           },
+//           {
+//             name: "Hirapur",
+//           },
+//         ],
+//       },
+//       {
+//         name: "Wasseypur",
+//         towns: [
+//           {
+//             name: "Sardar khan's",
+//           },
+//           {
+//             name: "Faizal khan's",
+//           },
+//         ],
+//       },
+//       {
+//         name: "Mirzapur",
+//         towns: [
+//           {
+//             name: "Kaleen bhaiya's",
+//           },
+//           {
+//             name: "Guddu bhaiya's",
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     name: "Assam",
+//     cities: [
+//       {
+//         name: "Guwhati",
+//         towns: [
+//           {
+//             name: "Amin",
+//           },
+//           {
+//             name: "Jalah",
+//           },
+//         ],
+//       },
+//       {
+//         name: "Jungle1",
+//         towns: [
+//           {
+//             name: "Tiger found at IIT Guwahati",
+//           },
+//           {
+//             name: "Leopard found in IIT Guwahati",
+//           },
+//         ],
+//       },
+//       {
+//         name: "Tezpur",
+//         towns: [
+//           {
+//             name: "Dibrugarh",
+//           },
+//           {
+//             name: "Silchar",
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     name: "Bihar",
+//     cities: [
+//       {
+//         name: "Patna",
+//         towns: [
+//           {
+//             name: "Sonpur",
+//           },
+//           {
+//             name: "Maner",
+//           },
+//         ],
+//       },
+//       {
+//         name: "Gaya",
+//         towns: [
+//           {
+//             name: "Bakraur",
+//           },
+//           {
+//             name: "Barachatti",
+//           },
+//         ],
+//       },
+//       {
+//         name: "Darbhanga",
+//         towns: [
+//           {
+//             name: "Singhwara",
+//           },
+//           {
+//             name: "Jale",
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ];
+
+// function App() {
+//   return <div id="main"></div>;
+// }
+
+// export default App;
